58317b3628fcceaefcd3e013388ad4bbadf0a29c
ViktorKozhuev <viktor.kozhuev@gmail.com>
2022-11-12 11:40:39 +0300

    added autodocumentation drf_yasg

--- django_movie/urls.py
+++ django_movie/urls.py
@@ -18,6 +18,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from .yasg import urlpatterns as doc_urls
+
 
 
 urlpatterns = [
@@ -30,6 +32,8 @@ urlpatterns = [
     path('api/v1/', include('movies.urls'))
 
 ]
+
+urlpatterns += doc_urls
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
